acc33c2714e9a0b8704f447eceac15230f44414b
affonja <affonja@yandex.ru>
2023-11-28 00:18:47 +0400

    stage3

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/affonja/php-project-48/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/affonja/php-project-48/actions)
+
+stage_3
+<a href="https://asciinema.org/a/cQbDpJTGc77MUsY951UALnhs2" target="_blank"><img src="https://asciinema.org/a/cQbDpJTGc77MUsY951UALnhs2.svg" /></a>
--- src/genDiff.php
+++ src/genDiff.php
@@ -4,10 +4,18 @@ namespace Differ;
 
 use function Functional\flatten;
 
-function genDiff($path1, $path2)
+define("ROOT_DIR", $_SERVER["PWD"]);
+define("DIR_SOURCE", ROOT_DIR . '/tests/');
+
+function genDiff(string $path1, string $path2): string
 {
+    $is_absolute_path = getTypePath($path1);
+    $path1 = getNormalizePath($path1, $is_absolute_path);
+    $is_absolute_path = getTypePath($path2);
+    $path2 = getNormalizePath($path2, $is_absolute_path);
+
     if (!file_exists($path1) && (!file_exists($path2))) {
-        throw new \Exception('Unknown file');
+        throw new \Exception('File not exist');
     }
     $file1 = (array)json_decode(file_get_contents($path1));
     $file2 = (array)json_decode(file_get_contents($path2));
@@ -16,31 +24,60 @@ function genDiff($path1, $path2)
     sort($keys);
     $keys = array_unique($keys);
 
+
     $diff = array_map(
         function ($key) use ($file2, $file1) {
             if (array_key_exists($key, $file1) && !array_key_exists($key, $file2)) {
-                $result[] = "- $key: $file1[$key]";
+                $result[] = "- $key: " . boolToString($file1[$key]);
             } elseif (!array_key_exists($key, $file1) && array_key_exists($key, $file2)) {
-                $result[] = "+ $key: $file2[$key]";
+                $result[] = "+ $key: " . boolToString($file2[$key]);
             } elseif (array_key_exists($key, $file1) && array_key_exists($key, $file2)) {
                 if ($file1[$key] === $file2[$key]) {
-                    $result[] = "  $key: $file1[$key]";
+                    $result[] = "  $key: " . boolToString($file1[$key]);
                 } else {
-                    $result[] = "- $key: $file1[$key]";
-                    $result[] = "+ $key: $file2[$key]";
+                    $result[] = "- $key: " . boolToString($file1[$key]);
+                    $result[] = "+ $key: " . boolToString($file2[$key]);
                 }
             }
-            return $result;
+            return $result ?? [];
         }
         , $keys
     );
     $diff = flatten($diff);
 
-//    return flatten($diff);
     return implode(PHP_EOL, $diff);
 }
 
-function validatePath($path)
+function getNormalizePath(string $path, bool $type): string
 {
-    
+    if ($type) {
+        if (stripos(pathinfo($path)['dirname'], realpath(ROOT_DIR)) === false) {
+            if ($path[0] === '/' || $path[0] === '\\') {
+                $path = ROOT_DIR . $path;
+            }
+        }
+    } else {
+        if (pathinfo($path)['dirname'] === '.') {
+            $path = DIR_SOURCE . $path;
+        } else {
+            $path = ROOT_DIR . '/' . $path;
+        }
+    }
+
+    return realpath($path) ?? '';
+}
+
+function getTypePath(string $path): bool
+{
+    $a = strspn($path, '/\\', 0, 1);
+    $b = strlen($path) > 3 && ctype_alpha($path[0]);
+    $c = substr($path, 1, 1) === ':';
+    $d = strspn($path, '/\\', 2, 1);
+
+    return $a || ($b && $c && $d);
+}
+
+function boolToString($val): string|int
+{
+    return is_bool($val) ? ($val === true ? 'true' : 'false') : $val;
 }
