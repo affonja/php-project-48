b6f62d85f52236a154566e64e66b6f4b78deeffd
affonja <56819481+affonja@users.noreply.github.com>
2024-01-31 22:34:48 +0400

    fix5

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -6,26 +6,5 @@ use function Differ\Formatters\Formatters\toString;
 
 function format(array $diff): bool|string
 {
-    $translate = [
-        'add' => "+",
-        'rmv' => '-',
-        'upd=' => ' ',
-        'upd-' => '-',
-        'upd+' => '+',
-        'nested' => ' '
-    ];
-
-    $formatDiff = function ($item) use (&$formatDiff, $translate) {
-        $new_action = $translate[$item['action']];
-        if (is_array($item['value'])) {
-            $new_value = array_map($formatDiff, $item['value']);
-        } else {
-            $new_value = toString($item['value']);
-        }
-        return array_merge($item, ['action' => $new_action, 'value' => $new_value]);
-    };
-
-    $result = array_map($formatDiff, $diff);
-
-    return json_encode($result);
+    return json_encode($diff);
 }
